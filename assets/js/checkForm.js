3576f4df8d97a17dd1fed2f22138363fbfd79001
projectnovruzzade <tunarwork99@gmail.com>
2024-07-19 18:57:20 +0400

    v.1.2

---
+++ assets/js/checkForm.js
@@ -0,0 +1,28 @@
+const all_inputs = document.querySelectorAll(".form_input");
+const loginBtn = document.querySelector(".login");
+
+function checkInputs() {
+    let allFilled = true;
+    for(let btn of all_inputs) {
+        if(btn.value.trim() === "") {
+            allFilled = false;
+            break;
+        }
+    }
+
+    if(allFilled) {
+        loginBtn.disabled = false;
+        loginBtn.classList.remove("disabled");
+    } else {
+        loginBtn.disabled = true;
+        loginBtn.classList.add("disabled");
+    }
+}
+
+checkInputs();
+
+for(let btn of all_inputs) {
+    btn.addEventListener("change", (e) => {
+        checkInputs();
+    });
+}
